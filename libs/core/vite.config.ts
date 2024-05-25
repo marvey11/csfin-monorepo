a91273c5542896871842be257d8c73279529644f
Marco Wegner <673439+marvey11@users.noreply.github.com>
2024-05-25 15:58:04 +0200

    chore: convert core to non-buildable lib

--- libs/core/vite.config.ts
+++ libs/core/vite.config.ts
@@ -1,8 +1,8 @@
 /// <reference types='vitest' />
+import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
+import * as path from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
-import * as path from "path";
-import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
 
 export default defineConfig({
   root: __dirname,
@@ -20,29 +20,6 @@ export default defineConfig({
   // worker: {
   //  plugins: [ nxViteTsPaths() ],
   // },
-
-  // Configuration for building your library.
-  // See: https://vitejs.dev/guide/build.html#library-mode
-  build: {
-    outDir: "../../dist/libs/core",
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-    lib: {
-      // Could also be a dictionary or array of multiple entry points.
-      entry: "src/index.ts",
-      name: "core",
-      fileName: "index",
-      // Change this to the formats you want to support.
-      // Don't forget to update your package.json as well.
-      formats: ["es", "cjs"],
-    },
-    rollupOptions: {
-      // External packages that should not be bundled into your library.
-      external: [],
-    },
-  },
 
   test: {
     globals: true,
