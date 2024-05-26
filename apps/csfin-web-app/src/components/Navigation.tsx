4d6bbf4cd1f749bc775f74d789c1cecaaf579552
Marco Wegner <673439+marvey11@users.noreply.github.com>
2024-05-26 08:35:08 +0200

    feat: add simple navigation

--- apps/csfin-web-app/src/components/Navigation.tsx
+++ apps/csfin-web-app/src/components/Navigation.tsx
@@ -2,7 +2,11 @@ import { NavigationLink } from "./NavigationLink";
 
 export const Navigation = () => {
   return (
+<<<<<<< HEAD
     <nav className="text-white bg-black flex justify-between items-stretch sticky top-0 px-4 h-[var(--navbar-height)]">
+=======
+    <nav className="text-white bg-black flex justify-between items-stretch sticky top-0 px-4">
+>>>>>>> d124524 (feat: add simple navigation)
       <span className="text-3xl font-black py-2">{"{csfin}"}</span>
 
       <ul className="p-0 m-0 list-none flex">
