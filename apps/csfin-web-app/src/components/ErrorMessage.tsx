f78d89eae4d52fb0d5c518005b9c4d7ba15648c9
Marco Wegner <673439+marvey11@users.noreply.github.com>
2024-05-26 17:18:32 +0200

    chore: add unsaved change

--- apps/csfin-web-app/src/components/ErrorMessage.tsx
+++ apps/csfin-web-app/src/components/ErrorMessage.tsx
@@ -5,10 +5,8 @@ interface ErrorMessageProps {
 }
 
 export const ErrorMessage = ({ children }: ErrorMessageProps) => (
-  <div className="flex justify-center p-4">
-    <div className="flex flex-col gap-4 w-1/2 bg-red-200 border-2 border-red-400 px-4 py-2 rounded-md shadow-md">
-      <span className="font-bold text-red-400">ERROR</span>
-      <span className="text-black">{children}</span>
-    </div>
+  <div className="flex flex-col gap-4 w-1/2 bg-red-200 border-2 border-red-400 px-4 py-2 rounded-md shadow-md">
+    <span className="font-bold text-red-400">ERROR</span>
+    <span className="text-black">{children}</span>
   </div>
 );
