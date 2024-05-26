22cbebe3edda110ba494f82708d12cae1ae07366
Marco Wegner <673439+marvey11@users.noreply.github.com>
2024-05-26 19:36:14 +0200

    feat(web): add local-storage hook

---
+++ apps/csfin-web-app/src/hooks/useLocalStorage.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export default function useLocalStorage<T>(
+  key: string,
+  defaultValue: T | (() => T)
+) {
+  const [value, setValue] = useState<T>(() => {
+    const jsonValue = localStorage.getItem(key);
+    if (jsonValue != null) {
+      return JSON.parse(jsonValue);
+    }
+
+    return typeof defaultValue === "function"
+      ? (defaultValue as () => T)()
+      : defaultValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue] as [T, typeof setValue];
+}
