306b533f75ca429617ac409bd8c35865fda2e806
Marco Wegner <673439+marvey11@users.noreply.github.com>
2026-08-06 16:35:09 +0200

    Update asset path in webpack configuration

--- apps/csfin-rest-api/webpack.config.js
+++ apps/csfin-rest-api/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = composePlugins(
     compiler: 'tsc',
     main: './src/main.ts',
     tsConfig: './tsconfig.app.json',
-    assets: ['./src/assets'],
+    assets: ['./apps/csfin-rest-api/src/assets'],
     optimization: false,
     outputHashing: 'none',
   }),
