83c704d0ad0521102e42facb11e9d84ec632e30f
Marco Wegner <673439+marvey11@users.noreply.github.com>
2026-08-06 16:19:37 +0200

    Refactor webpack config to use composePlugins

--- apps/csfin-rest-api/webpack.config.js
+++ apps/csfin-rest-api/webpack.config.js
@@ -1,21 +1,21 @@
-
-const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+const { composePlugins, withNx } = require('@nx/webpack');
 const { join } = require('path');
 
-module.exports = {
-  output: {
-    path: join(__dirname, '../../dist/apps/csfin-rest-api'),
-  },
-  plugins: [
-    new NxAppWebpackPlugin({
-      target: 'node',
-      compiler: 'tsc',
-      main: './src/main.ts',
-      tsConfig: './tsconfig.app.json',
-      assets: ["./src/assets"],
-      optimization: false,
-      outputHashing: 'none',
-    })
-  ],
-};
-
+module.exports = composePlugins(
+  withNx({
+    target: 'node',
+    compiler: 'tsc',
+    main: './src/main.ts',
+    tsConfig: './tsconfig.app.json',
+    assets: ['./src/assets'],
+    optimization: false,
+    outputHashing: 'none',
+  }),
+  (config) => {
+    config.output = {
+      ...config.output,
+      path: join(__dirname, '../../dist/apps/csfin-rest-api'),
+    };
+    return config;
+  }
+);
