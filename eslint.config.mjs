9bd5b21d810b8f8a76c653b89dddea48e3b278c6
Marco Wegner <673439+marvey11@users.noreply.github.com>
2026-08-13 10:18:31 +0000

    chore: run npm upgrade

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
 import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import js from "@eslint/js";
 import nx from "@nx/eslint-plugin";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const compat = new FlatCompat({
   baseDirectory: dirname(fileURLToPath(import.meta.url)),
@@ -11,34 +11,25 @@ const compat = new FlatCompat({
 
 export default [
   ...nx.configs["flat/base"],
-  ...compat
-    .config({
-      extends: [
-        "plugin:@typescript-eslint/strict",
-        "plugin:@typescript-eslint/stylistic",
-      ],
-    })
-    .map((config) => ({
-      ...config,
-      files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
-      rules: {
-        ...config.rules,
-        "@nx/enforce-module-boundaries": [
-          "error",
-          {
-            enforceBuildableLibDependency: true,
-            allow: [],
-            depConstraints: [
-              {
-                sourceTag: "*",
-                onlyDependOnLibsWithTags: ["*"],
-              },
-            ],
-          },
-        ],
-      },
-    })),
   ...nx.configs["flat/typescript"],
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+    rules: {
+      "@nx/enforce-module-boundaries": [
+        "error",
+        {
+          enforceBuildableLibDependency: true,
+          allow: [],
+          depConstraints: [
+            {
+              sourceTag: "*",
+              onlyDependOnLibsWithTags: ["*"],
+            },
+          ],
+        },
+      ],
+    },
+  },
   {
     files: ["**/*.ts", "**/*.tsx"],
     rules: {
